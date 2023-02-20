a6844dd3ca3b97c7fafce6ddec02c39867c4c0e2
Joshua Johnson <joshuatjohnson255@gmail.com>
2023-02-20 19:05:40 +0530

    This is the Master Branch With Main TopBar

---
+++ src/components/TopBarMain/TopBarMain.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './TopBarMain.css';
+import { useNavigate } from 'react-router-dom';
+
+function TopBarMain() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Navbar className="color_nav">
+        <Container>
+          <Nav className="m-auto">
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/cars');
+              }}
+            >
+              Cars
+            </Navbar.Brand>
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/signup');
+              }}
+            >
+              Signup
+            </Navbar.Brand>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default TopBarMain;
