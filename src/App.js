bb5a8542e77b8f74d1c34f9253cb4feddaaa9ffa
Joshua Johnson <joshuatjohnson255@gmail.com>
2023-02-20 19:17:33 +0530

    Added Banner Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TopBarMain from './components/TopBarMain/TopBarMain';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
+          <Banner />
           <TopBarMain />
           <Routes>
             <Route />
